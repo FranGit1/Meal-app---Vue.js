9e37d8966bede8ca66bbd03bbaab8517ab919f5b
FranGit1 <marinicfran955@gmail.com>
2023-04-18 17:53:10 +0200

    initial commit

---
+++ meal-app/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col items-center p-8 justify-center">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for meals"
+    />
+    <div class="flex gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+        >{{ letter }}</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from "vue";
+import store from "../store";
+import axiosClient from "../axiosClient";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+onMounted(() => {
+  const response = axiosClient.get("/list.php?i=list");
+  console.log(response);
+});
+</script>
