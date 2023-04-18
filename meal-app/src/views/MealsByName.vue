d075534c8fb0091be58952edbc8d29353fe95c2f
FranGit1 <marinicfran955@gmail.com>
2023-04-18 18:40:56 +0200

    If this commit is applied search meals by name will be implemented

--- meal-app/src/views/MealsByName.vue
+++ meal-app/src/views/MealsByName.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>By Name</div>
+  <div class="p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals by Name"
+      @change="searchMeals"
+    />
+  </div>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <router-link to="/">
+        <img
+          :src="meal.strMealThumb"
+          :alt="strMeal"
+          class="rounded-t-xl w-full h-52 object-cover"
+        />
+      </router-link>
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          veritatis officia assumenda ipsa voluptates similique.
+        </p>
+        <div class="">
+          <a
+            class="px-3 py-2 rounded border-2 border-red-600 hover:bg-red-600 hover:text-white transition-colors"
+            :href="meal.strYoutube"
+            target="_blank"
+            >Youtube</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from "@vue/reactivity";
+import { useRoute } from "vue-router";
+import { ref, onMounted } from "vue";
+import store from "../store";
+
+const route = useRoute();
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  store.dispatch("searchMeals", keyword.value);
+}
+
+onMounted(() => {
+  keyword.value = route.params.name;
+  if (keyword.value) {
+    searchMeals();
+  }
+});
+</script>
