a52333dde4363d179571d9759efc8a8735c7251e
FranGit1 <marinicfran955@gmail.com>
2023-04-21 16:09:39 +0200

    Fixed bug when searching ano no results displays

--- meal-app/src/components/Meals.vue
+++ meal-app/src/components/Meals.vue
@@ -2,9 +2,13 @@
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
     <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
-  <div v-if="!meals.length" class="flex justify-center text-gray-600 p-8">
-    Search for meals to display them
+  <div v-if="meals === null" class="flex justify-center text-gray-600 p-8">
+    No meals that start with that name
   </div>
+
+  <!-- <div v-if="!meals.length" class="flex justify-center text-gray-600 p-8">
+    Search for meals to display them
+  </div> -->
 </template>
 
 <script setup>
